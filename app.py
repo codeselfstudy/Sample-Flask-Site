9685b56eb4ac771eb915859533da6dcdfa8e0179
J. Cohen <hostelmarketing@gmail.com>
2015-08-18 13:11:11 -0700

    Add some routes

--- app.py
+++ app.py
@@ -2,7 +2,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.routes('/wiki/Virtualenv')
+@app.route('/')
+def index():
+    return 'Saluton, Mondo!'
+
+@app.route('/page/<int:page_id>')
+def pages(page_id):
+    # Here you could do a database query to find the page with the ID of
+    # `page_id`
+    return 'You are viewing page #{}'.format(page_id)
+
+@app.route('/wiki/Virtualenv')
 def virtualenv_whatever_you_want():
     return 'This is a page about Virtualenvs!!!'
 
